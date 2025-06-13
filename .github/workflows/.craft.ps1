a413b04865807e04d125ad53ff91f5b67458a11b
Andrey <ltsolo@gmail.com>
2025-06-13 14:01:33 +0200

    fix(ci): update Craft script to use correct paths on Windows

--- .github/workflows/.craft.ps1
+++ .github/workflows/.craft.ps1
@@ -7,11 +7,14 @@ if ($IsWindows) {
 }
 
 $RepoRoot = "{0}/../../" -f ([System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition))
-$command = @("${env:HOME}/craft/CraftMaster/CraftMaster/CraftMaster.py",
+$craftMasterPath = if ($IsWindows) { "${env:USERPROFILE}/craft/CraftMaster/CraftMaster.py" } else { "${env:HOME}/craft/CraftMaster/CraftMaster.py" }
+$workspacePath = if ($IsWindows) { "${env:USERPROFILE}/craft" } else { "${env:HOME}/craft" }
+
+$command = @($craftMasterPath,
              "--config", "${RepoRoot}/.craft.ini",
              "--config-override", "${RepoRoot}/.github/workflows/craft_override.ini",
              "--target", "${env:CRAFT_TARGET}",
-             "--variables", "WORKSPACE=${env:HOME}/craft") + $args
+             "--variables", "WORKSPACE=$workspacePath") + $args
 
 Write-Host "Exec: ${python} ${command}"
 
